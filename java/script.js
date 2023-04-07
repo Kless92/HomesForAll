40223a7b1960f400aeb5ce913192e393e49a0445
Spencer Klessens <klessens92@gmail.com>
2023-04-07 11:04:17 -0500

    update 3 HomesforAll

--- java/script.js
+++ java/script.js
@@ -12,5 +12,38 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     })
+    /*$('.form-check-input').click(function(e) {
+        e.preventDefault();
+        var radio=$(this).find('input[type=radio]');
+        if(radio.is(':checked')) {
+        $(this).removeClass("active");
+        radio.removeProp('checked', false);
+        } 
+        else {
+            radio.prop('checked', true);
+        }
+        console.log("aaa")
+    })*/
+    $('#twenty').click(function(e){
+        $('#fifty').prop('checked', false);
+        $('#hundred').prop('checked', false);
+        $('#twoHundred').prop('checked', false);
+    })
+    $('#fifty').click(function(e){
+        $('#twenty').prop('checked', false);
+        $('#hundred').prop('checked', false);
+        $('#twoHundred').prop('checked', false);
+    })
+    $('#hundred').click(function(e){
+        $('#fifty').prop('checked', false);
+        $('#twenty').prop('checked', false);
+        $('#twoHundred').prop('checked', false);
+    })
+    $('#twoHundred').click(function(e){
+        $('#fifty').prop('checked', false);
+        $('#hundred').prop('checked', false);
+        $('#twenty').prop('checked', false);
+    })
+
 });
 
